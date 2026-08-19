d706899e32cfe1e9bd275c2b4862c42bd44acf17
weeny-V <48623554+weeny-V@users.noreply.github.com>
2026-08-19 18:32:28 +0300

    delpoy fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -79,7 +79,7 @@ export default function Navbar() {
         <Tooltip>
           <TooltipTrigger asChild>
             <DockIcon className="rounded-3xl cursor-pointer size-full bg-background p-0 text-muted-foreground hover:text-foreground hover:bg-muted backdrop-blur-3xl border border-border transition-colors">
-              <ModeToggle className="size-full cursor-pointer" />
+              <ModeToggle />
             </DockIcon>
           </TooltipTrigger>
           <TooltipContent
